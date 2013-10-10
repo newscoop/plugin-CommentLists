9cc65f9d0ee7a82fb40d4c3ce4e8d00c4268d9a9
Rafał Muszyński <rmuszynski1@gmail.com>
2013-10-10 13:48:51 +0200

    first commit

---
+++ Controller/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package Newscoop\CommentListsBundle
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2013 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\CommentListsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminController extends Controller
+{
+    /**
+    * @Route("/plugin/comment-lists")
+    * @Template()
+    */
+    public function indexAction(Request $request)
+    {  
+        return array();
+    }
+}
