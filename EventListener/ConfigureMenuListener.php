1ba64973ed84cf32c77e5df4add746ab419e92ce
Rafał Muszyński <rmuszynski1@gmail.com>
2013-10-10 13:50:31 +0200

    first commit

--- EventListener/ConfigureMenuListener.php
+++ EventListener/ConfigureMenuListener.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package Newscoop\ArticlesCalendarBundle
+ * @package Newscoop\CommentListsBundle
  * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
  * @copyright 2013 Sourcefabric o.p.s.
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-namespace Newscoop\ArticlesCalendarBundle\EventListener;
+namespace Newscoop\CommentListsBundle\EventListener;
 
 use Newscoop\NewscoopBundle\Event\ConfigureMenuEvent;
 use Symfony\Component\Translation\Translator;
