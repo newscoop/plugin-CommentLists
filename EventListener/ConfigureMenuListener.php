b922fe0863598c13b7de0c8dc484fd6c8f9c6a37
Rafał Muszyński <rmuszynski1@gmail.com>
2013-10-10 13:47:36 +0200

    first commit

---
+++ EventListener/ConfigureMenuListener.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Newscoop\ArticlesCalendarBundle
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2013 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\ArticlesCalendarBundle\EventListener;
+
+use Newscoop\NewscoopBundle\Event\ConfigureMenuEvent;
+use Symfony\Component\Translation\Translator;
+
+class ConfigureMenuListener
+{
+    private $translator;
+
+    /**
+     * @param Translator $translator
+     */
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param ConfigureMenuEvent $event
+     */
+    public function onMenuConfigure(ConfigureMenuEvent $event)
+    {
+        $menu = $event->getMenu();
+
+        $menu[$this->translator->trans('Plugins')]->addChild(
+            $this->translator->trans('plugin.label.title'), 
+            array('uri' => $event->getRouter()->generate('newscoop_articlescalendar_admin_admin'))
+        );
+    }
+}
---
+++ NewscoopCommentListsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Newscoop\CommentListsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NewscoopCommentListsBundle extends Bundle
+{
+}
